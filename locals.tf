d39a5c375a16c36256ccd40a265451897ff0016b
Joakim Olsson <joakim@unbound.se>
2024-11-20 09:28:56 +0100

    chore: bump module version
    Made with ❤️️ by updatecli

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   # x-release-please-version
-  version = "1.6.7"
+  version = "1.6.8"
   yaml = templatefile("${path.module}/k8s-monitoring.yaml.tftpl", {
     cluster_name         = var.cluster_name
     logs_secret          = var.logs_secret
