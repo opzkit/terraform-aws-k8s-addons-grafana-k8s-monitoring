78151e5bf96dfaf560c25c5391299ac391b6e76c
Peter Svensson <peter@sparetimecoders.com>
2024-11-20 14:24:14 +0100

    chore(main): release 0.2.0 (#23)

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   // x-release-please-start-version
-  version = "0.1.1"
+  version = "0.2.0"
   // x-release-please-end
   yaml = templatefile("${path.module}/k8s-monitoring.yaml.tftpl", {
     cluster_name         = var.cluster_name
